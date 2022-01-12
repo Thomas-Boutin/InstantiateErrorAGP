56d801d177de1111ac5e6db7c39b821b078d85ab
tboutin <tboutin@ippon.fr>
2022-01-12 22:25:07 +0100

    feat(issue): create sample project which demonstrates lint issues

---
+++ app/src/main/java/fr/sample/instantiateerroragp/MainActivity.kt
@@ -0,0 +1,5 @@
+package fr.sample.instantiateerroragp
+
+import fr.sample.intermediate.BaseFirstParentActivity
+
+class MainActivity : BaseFirstParentActivity()
---
+++ intermediate/src/main/java/fr/sample/intermediate/BaseFirstParentActivity.kt
@@ -0,0 +1,5 @@
+package fr.sample.intermediate
+
+import androidx.appcompat.app.AppCompatActivity
+
+abstract class BaseFirstParentActivity : AppCompatActivity()
